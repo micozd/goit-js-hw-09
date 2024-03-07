079e828d620764369d82769de6c2f267a0ae1e6e
micozd <ozdarskimichal@gmail.com>
2024-03-07 19:24:24 +0100

    rozwiązane

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+function getRandomHexColor() {  
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    }
 
+function changeBackgroundColor() {
+
+    document.body.style.backgroundColor = getRandomHexColor();
+    timeout = setTimeout(changeBackgroundColor, 1000);
+    
+}
+
+function pause() {
+    clearTimeout(timeout)
+    timeout = null
+}
+    
+
+
+document.querySelector('button[data-start]').addEventListener('click', function() {
+    this.disabled = true;
+    document.querySelector('button[data-stop]').disabled = false;
+    changeBackgroundColor();
+});
+
+document.querySelector('button[data-stop]').addEventListener('click', function() {
+    this.disabled = true;
+    document.querySelector('button[data-start]').disabled = false;
+    pause();
+});
