108343045a5724c1487b04c1997cd481bd59d1c1
micozd <125865934+micozd@users.noreply.github.com>
2024-03-08 12:07:57 +0000

    Deploying to gh-pages from @ micozd/goit-js-hw-09@6b828108fe676c1801ccae07429393fad47df2cb 🚀

---
+++ 01-color-switcher.399d6c17.js
@@ -0,0 +1,2 @@
+var t=null;function e(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`,t=setTimeout(e,1e3)}document.querySelector("button[data-start]").addEventListener("click",(function(){this.disabled=!0,document.querySelector("button[data-stop]").disabled=!1,e()})),document.querySelector("button[data-stop]").addEventListener("click",(function(){return this.disabled=!0,document.querySelector("button[data-start]").disabled=!1,clearTimeout(t),t=null,t=null}));
+//# sourceMappingURL=01-color-switcher.399d6c17.js.map
